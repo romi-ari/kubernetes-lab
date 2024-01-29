f258b846ee1b4073b972e89fde952143fefbec3b
romiari <romiari777@gmail.com>
2024-01-29 13:41:05 +0700

    fix error line 2 vpc.tf

--- terraform/digitalocean/1-kubernetes.tf
+++ terraform/digitalocean/1-kubernetes.tf
@@ -1,14 +1,18 @@
-resource "digitalocean_kubernetes_cluster" "romo_cluster" {
-  name          = "romo_cluster"
+resource "digitalocean_kubernetes_cluster" "romo" {
+  name          = "romo-cluster"
   region        = "sgp1"
   auto_upgrade  = false
   version       = "1.29.0-do.0"
   vpc_uuid      = digitalocean_vpc.romo.id
+  tags          = ["kube-lab"]
 
   node_pool {
     name       = "worker-pool"
     size       = "s-2vcpu-4gb"
     auto_scale = false
     node_count = 3
+    labels     = {
+      "service" = "kube-lab"
+    }  
   }
 }
--- terraform/digitalocean/2-vpc.tf
+++ terraform/digitalocean/2-vpc.tf
@@ -1,5 +1,5 @@
 resource "digitalocean_vpc" "romo" {
-  name     = "romo_vpc"
+  name     = "romo-vpc"
   region   = "sgp1"
   ip_range = "192.168.1.0/24"
 }
